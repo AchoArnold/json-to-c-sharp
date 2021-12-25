f3ba70716866670fc56084698f6b783249fdbf91
Acho Arnold <arnold@ndolestudio.com>
2021-12-25 22:55:31 +0200

    Bring brack redirect on local enviornment

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -13,7 +13,12 @@ builder.Services.AddApplicationInsightsTelemetry();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Error");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
 
 app.UseStaticFiles();
 
